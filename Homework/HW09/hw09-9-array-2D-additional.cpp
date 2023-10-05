4ce86d79c79a77821926cd907d5e0d6071a84571
Jirphat Saetae <137375099+JirphatAok@users.noreply.github.com>
2023-10-05 13:16:47 +0700

    000

--- Homework/HW09/hw09-9-array-2D-additional.cpp
+++ Homework/HW09/hw09-9-array-2D-additional.cpp
@@ -24,48 +24,52 @@
 #include <stdio.h>
 
 int main() {
-    int high , high_2 ;
-    int Long , Long_2 ;
 
-    printf("Array1 element:\n");
-    scanf("%d %d", &high, &Long);
+    int i, j, row1, cols1, row2, cols2;
 
-    int matrix[high][Long];
+    printf("Array1 element :\n") ;
+    scanf( "%d %d", &row1, &cols1 ) ;
 
-    printf("Input :\n");
-    for ( int i = 0 ; i < high ; i++ ) {
-        for ( int j = 0 ; j < Long ; j++ ) {
-            scanf("%d", &matrix[i][j]) ;
+    int matrix[ row1 ][ cols1 ] ;
+
+    printf( "Input :\n" ) ;
+    for ( i = 0 ; i < row1 ; i++ ){
+        for ( j = 0 ; j < cols1 ; j++ ) {
+            scanf( "%d", &matrix[ i ][ j ] ) ;
         }
     }
 
-    printf( "Array2 element:\n" ) ;
-    scanf( "%d %d", &high_2, &Long_2 ) ;
+    printf("Array2 element :\n") ;
+    scanf( "%d %d", &row2, &cols2 ) ;
 
-    int matrix_2[high_2][Long_2];
+    int matrix2[ row2 ][ cols2 ] ;
 
-    printf("Input :\n");
-
-    for ( int i = 0 ; i < high_2 ; i++ ) {
-        for (int j = 0 ; j < Long_2 ; j++) {
-            scanf( "%d", &matrix_2[ i ][ j ] ) ;
+    printf( "Input :\n" ) ;
+    for ( i = 0 ; i < row2 ; i++ ){
+        for ( j = 0 ; j < cols2 ; j++ ) {
+            scanf( "%d", &matrix2[ i ][ j ] ) ;
         }
     }
 
-    int matrix_3[ high ][ Long ];
+    int matrix3[ row1 ][ cols1 ] ;
 
-    for (int i = 0; i < high; i++) {
-        for (int j = 0; j < Long; j++) {
-            matrix_3[i][j] = matrix[i][j] + matrix_2[i][j];
+    // printf( "Input :" ) ;
+    for ( i = 0 ; i < row1 ; i++ ){
+        for ( j = 0 ; j < cols1 ; j++ ) {
+            matrix3[ i ][ j ] = matrix[ i ][ j ] + matrix2[ i ][ j ];
         }
     }
-    printf("Array1 + Array2 = Array3\n");
-        for (int i = 0; i < high; i++) {
-            for (int j = 0; j < Long; j++) {
-                printf("%d ", matrix_3[i][j]);
+
+    printf( "Array1 + Array2 = Array3\n" ) ;
+    for ( i = 0 ; i < row1 ; i++ ) {
+            for ( j = 0 ; j < cols1 ; j++ ) {
+                printf( "%d", matrix3[ i ][ j ] ) ;
+                if ( cols1 >  j + 1 ) {
+                    printf(" ") ;
+                }
             }
-            printf("\n");
+            printf("\n") ;
         }
 
-    return 0;
+    return 0 ;
 }
